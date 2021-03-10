4c44a11dc39f77e1bbafa1c5afb42ee3897e82db
koooyooo <jab.cross.hook@gmail.com>
2021-03-11 01:04:13 +0900

    fix readme

--- singleton/singleton_test.go
+++ singleton/singleton_test.go
@@ -8,15 +8,8 @@ import (
 	"github.com/koooyooo/go-design-pattern/singleton/singleton"
 )
 
-// Singleton は唯一のインスタンスを生成するパターンです。具体的には以下の特徴でこれを実現します。
-// - 外部から直接生成できなくし、替わりに参照取得用の関数を用意する
-// - その関数から何度取得しても同じ参照が返る
-// Singleton を適用することで、次の様なことが実現できます。
-// - Enumの様に structで種別を表現する
-// - Flyweightパターンとして Instance生成の負荷を抑えて軽量化する
-// また、DIコンテナの中ではこの様なコード的な制約は持たせませんが Serviceや Repository等のコンポーネントを1つだけ生成します。
-// この様に、デザインパターンとして適用するSingletonだけではなく、事実上Singletonが成立する場合もあり、シンプルな分より理想的です。
-// Singletonはクラス(struct)やインターフェイス同士の組み合わせによるパターンというよりは、struct内部のパターンとなります。
+// Singletonは唯一のインスタンスを用意し、それ以外を生成できなくするパターンです。
+// そのため、唯一のインスタンスしか存在しないことを保証できます。
 func TestSingleton(t *testing.T) {
 	// Instance()以外で生成できない => singleton.singleton{} は不可能
 	s1 := singleton.Instance()
