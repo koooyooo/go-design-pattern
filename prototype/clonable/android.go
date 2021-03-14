e2d24e683d48026b73ff453da4a70626148667ff
koooyooo <jab.cross.hook@gmail.com>
2021-03-14 23:46:46 +0900

    add some comment

---
+++ prototype/clonable/android.go
@@ -0,0 +1,14 @@
+package clonable
+
+// Android はClone可能な構造体
+type Android struct {
+	Name string
+	Log  []string
+}
+
+// 自身の複製を生成するメソッド
+func (a Android) Clone() Android {
+	// Goでは値(非ポインタ)の代入だけでクローン可能
+	b := a
+	return b
+}
--- prototype/prototype_test.go
+++ prototype/prototype_test.go
@@ -1,23 +1,35 @@
+// [目的]
+// Prototypeパターンの目的はインスタンスの複製の省力化です
+//
+// [概要]
+// 特に内部で保持している状態が多い、または複雑なインスタンスの場合、状態を複製するのが大変です。
+// しかし手書きでの複製が大変でもコピーが楽な局面は多いものです。Prototypeはコピーしてインスタンスを生成することにあります。
+//
+// [実現]
+// Javaの場合は全てのインスタンスはポインタなので、言語特有の Object#clone()をコールします。
+// Golangの場合はstructがポインタとは限らないので、参照先の struct値を別変数にコピーすることで実現可能です。
 package prototype_pattern
 
 import (
 	"testing"
 
+	"github.com/koooyooo/go-design-pattern/prototype/clonable"
+
 	"github.com/stretchr/testify/assert"
 )
 
-type Android struct {
-	Name string
-}
-
 func TestPrototype(t *testing.T) {
-	a := Android{
+	// 構造体"Android"を生成し状態を作り込む
+	a := clonable.Android{
 		Name: "Droid",
+		Log:  []string{"Walk", "Sleep", "Sleep Twice"},
 	}
-	// 非ポインタの参照を渡すだけでクローン可能
-	b := a
+	// Cloneを作成することで状態の複製を簡易化
+	b := a.Clone()
 	assert.Equal(t, "Droid", b.Name)
+	assert.ElementsMatch(t, []string{"Walk", "Sleep", "Sleep Twice"}, b.Log)
 
+	// 片系統の値を変更してももう一方に影響が無いことを確認
 	b.Name = "AnDroid"
 	assert.Equal(t, "Droid", a.Name)
 	assert.Equal(t, "AnDroid", b.Name)
