d21b38895623568639ee9d805b7aa2661ef1b1a1
koooyooo <jab.cross.hook@gmail.com>
2021-03-17 01:56:21 +0900

    add some comments

--- adapter/adapter/adapter.go
+++ adapter/adapter/adapter.go
@@ -8,10 +8,12 @@ import (
 
 type (
 	// レガシーAPI
+	// (httpパッケージを活用せずに実装)
 	LegacyAPI interface {
 		Receive(method, url string, header map[string][]string, body []byte) (string, error)
 	}
 	// モダンAPI
+	// (httpパッケージを活用して実装)
 	ModernAPI interface {
 		Receive(r *http.Request) (string, error)
 	}
@@ -25,11 +27,13 @@ func (l LegacyAPIImpl) Receive(method, url string, header map[string][]string, b
 }
 
 // Adapter定義
+// 委譲対象の LegacyAPIを内包している点が特徴
 type Adapter struct {
 	Legacy LegacyAPI
 }
 
 // 変換処理の実装
+// LegacyAPIを上手に活用して機能を実現
 func (a Adapter) Receive(r *http.Request) (string, error) {
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
