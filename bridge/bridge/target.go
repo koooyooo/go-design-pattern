a05bac81f4a362a3a807c5cec968f95d97957e7e
koooyooo <jab.cross.hook@gmail.com>
2021-03-18 01:07:50 +0900

    add some comments

--- bridge/bridge/target.go
+++ bridge/bridge/target.go
@@ -6,24 +6,18 @@ import (
 )
 
 type (
+	// WritingTarget は出力対象を表現します
 	WritingTarget struct {
-		Write  func(b []byte) (string, error)
+		// 抽象メソッド相当の関数
+		Write
+		// 委譲先のフォーマット
 		Format WritingFormat
 	}
+	// 出力関数
+	Write func(b []byte) (string, error)
 )
 
-func WriteStdout(b []byte) (string, error) {
-	s := fmt.Sprintf("[stdout] %s", string(b))
-	_, err := fmt.Fprint(os.Stdout, s)
-	return s, err
-}
-
-func WriteStderr(b []byte) (string, error) {
-	s := fmt.Sprintf("[stderr] %s", string(b))
-	_, err := fmt.Fprint(os.Stderr, s)
-	return s, err
-}
-
+// WriteOut は自身の抽象メソッド(関数)と委譲先のフォーマットを活用して出力を行います
 func (wb WritingTarget) WriteOut(v interface{}) (string, error) {
 	b, err := wb.Format.Marshal(v)
 	if err != nil {
@@ -34,4 +28,18 @@ func (wb WritingTarget) WriteOut(v interface{}) (string, error) {
 		return "", err
 	}
 	return s, nil
+}
+
+// 出力関数の標準出力実装
+var WriteStdout Write = func(b []byte) (string, error) {
+	s := fmt.Sprintf("[stdout] %s", string(b))
+	_, err := fmt.Fprint(os.Stdout, s)
+	return s, err
+}
+
+// 出力関数の標準エラー出力実装
+var WriteStderr Write = func(b []byte) (string, error) {
+	s := fmt.Sprintf("[stderr] %s", string(b))
+	_, err := fmt.Fprint(os.Stderr, s)
+	return s, err
 }
