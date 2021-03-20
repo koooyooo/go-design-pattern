a9f25735964e7b36eba46d2d8f865ac1ba9ddca8
koooyooo <jab.cross.hook@gmail.com>
2021-03-20 23:52:59 +0900

    add comments

--- x_guarded_suspension/taskqueue/taskqueue.go
+++ x_guarded_suspension/taskqueue/taskqueue.go
@@ -2,18 +2,21 @@ package taskqueue
 
 import "sync"
 
+// taskQueue は同期制御機能を持ったQueueです
 type taskQueue struct {
 	cond  *sync.Cond
 	tasks []interface{}
 	logs  []string
 }
 
+// NewTaskQueue は新規のtaskQueueを生成します
 func NewTaskQueue() *taskQueue {
 	return &taskQueue{
 		cond: sync.NewCond(&sync.Mutex{}),
 	}
 }
 
+// AddLast はtaskQueueの最後に要素を追加します
 func (q *taskQueue) AddLast(task interface{}) {
 	q.cond.L.Lock()
 	defer q.cond.L.Unlock()
@@ -23,6 +26,7 @@ func (q *taskQueue) AddLast(task interface{}) {
 	q.cond.Signal()
 }
 
+// RemoveFirst はtaskQueueの先頭の要素を削除しそれを返します
 func (q *taskQueue) RemoveFirst() interface{} {
 	q.cond.L.Lock()
 	defer q.cond.L.Unlock()
@@ -37,6 +41,7 @@ func (q *taskQueue) RemoveFirst() interface{} {
 	return v
 }
 
+// Logs はログを返します
 func (q taskQueue) Logs() []string {
 	return q.logs
 }
