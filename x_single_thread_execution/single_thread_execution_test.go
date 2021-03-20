d73a00a5ad8ef7bc9a10ddcf2eea3620daea50c6
koooyooo <jab.cross.hook@gmail.com>
2021-03-20 13:25:40 +0900

    add single thread execution comments

--- x_single_thread_execution/single_thread_execution_test.go
+++ x_single_thread_execution/single_thread_execution_test.go
@@ -1,8 +1,32 @@
 // [目的]
-// SingleThreadExecutionの目的は、複数Threadの同時実行による内部状態の不整合を抑止することです。
-// このパターンを用いないと、対象スコープに複数Threadが入り込みます。
+// SingleThreadExecutionの目的は、複数Threadの同時実行による不整合を抑止することです。
+// 何の不整合かというと、主に対象のメソッドが更新する状態の不整合となります。
 //
+// [概要]
+// 例えば銀行口座の取り扱いを例に取ります。銀行口座には残高(Balance)という内部状態があります。
+// この残高を1件1件の更新リクエスト毎に順番に更新してゆけば、問題は起こらないのですが、これが一気に更新すると事情が異なります。
+// 例えば、100人の銀行員が同じ瞬間に同じ口座を更新すると、処理途中でまだ最終確定していない残高に対して計算処理をしてしまったり、
+// 同じ残高を同時編集しようとして、編集が上手く反映されなかったりします。
+// この様な問題は、僅かなブレが許される傾向調査などであれば問題ないのですが、お金のやり取りでは許容できません。
 //
+// 解決策の一つが SingleThreadExecution, つまり整合性を担保するフローにおいては1度に1-Threadのみ通過を許すことです。
+// 複数人からの書き込みで問題が起きるのであれば、複数人からの書き込みを禁止してしまおうという発想です。
+// 昔の銀行では、複数銀行員が同じ口座台帳に対して同時に書き込むのは困難だったと思いますが、これも意図せず実装された SingleThreadExecutionであるわけです。
+//
+// [実現方法]
+// Javaでは Methodに synchronized修飾子を付けたり、synchronizedブロックを宣言したりしてSingleThreadの区間を宣言しました。
+// Goでは sync.Mutexの Lock()と Unlock()により同等のスコープを宣言します。
+// 「整合性を担保すべき一連の状態更新はそのスコープの中で完結させる」という点は一緒です。
+//
+// [シナリオ]
+// 残高 1000の銀行口座に対し、同時に100の処理Threadで更新をかけます。
+// 1回の更新内容は +100と -100という一連の操作です。理論的には最終的な残高は変化しません。
+// しかし、高い確率で SingleThreadExecutionを実装していないと、不整合が発生してしまいます。
+//
+// (これは、Thread-Aが更新途中の値を元にThread-Bが計算してしまうというレースコンディションに起因するものや、
+// メモリ上の同じ値を同時編集することに起因するものがあります。)
+//
+// そこで、一連のTransactionを表現するスコープにおいて、SingleThreadExecutionを実装して不整合を防いでいます。
 package x_single_thread_execution_pattern
 
 import (
@@ -17,6 +41,7 @@ import (
 	"github.com/koooyooo/go-design-pattern/x_single_thread_execution/synchronized"
 )
 
+// SingleThreadExecutionを実装していると不整合はない
 func TestSingleThreadExecution(t *testing.T) {
 	const numTx = 100
 
@@ -24,7 +49,7 @@ func TestSingleThreadExecution(t *testing.T) {
 	var w sync.WaitGroup
 	w.Add(numTx)
 
-	// 残高1000の口座に対し非同期で100回 +100 -100の取引を実施
+	// 残高1000の口座に対し 100Threadから同時に+100,-100の取引を実施
 	a := synchronized.NewAccount(&w, 1000)
 	for i := 0; i < numTx; i++ {
 		go a.PlusMinusTransaction(100, 100)
@@ -40,6 +65,7 @@ func TestSingleThreadExecution(t *testing.T) {
 	fmt.Printf("Single: %b\n", a.Amount())
 }
 
+// SingleThreadExecutionを実装していないと高い確率で不整合が発生する
 func TestNonSingleThreadExecution(t *testing.T) {
 	const numTx = 100
 
@@ -47,7 +73,7 @@ func TestNonSingleThreadExecution(t *testing.T) {
 	var w sync.WaitGroup
 	w.Add(numTx)
 
-	// 残高1000の口座に対し非同期で100回 +100 -100の取引を実施
+	// 残高1000の口座に対し 100Threadから同時に+100,-100の取引を実施
 	a := un_synchronized.NewAccount(&w, 1000)
 	for i := 0; i < numTx; i++ {
 		go a.PlusMinusTransaction(100, 100)
