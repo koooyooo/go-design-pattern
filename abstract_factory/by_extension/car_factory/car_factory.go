c9e717c063d76cee58d18f096d5409ae702b1536
koooyooo <jab.cross.hook@gmail.com>
2022-07-04 11:37:26 +0900

    add comment

--- abstract_factory/by_extension/car_factory/car_factory.go
+++ abstract_factory/by_extension/car_factory/car_factory.go
@@ -12,7 +12,10 @@ type (
 	// 本来のAbstractFactoryは継承なので、①親の型への代入・②テンプレートフローの提供 の双方が行なえますが、
 	// Golangで①を interfaceで実現した場合、②のテンプレートメソッドとなる関数は実装できないので、基底型を用意する必要があります
 	abstractCarFactoryImpl struct {
-		// 製造過程の関数群も、実際は子クラスでのOverrideを用いますが、interfaceでの切り替えで実現します。
+		// 製造過程の関数群も、本来は子クラスでのOverrideを用いますが、実際は interfaceの切り替えで実現します。
+		// 単純な Overrideで実現できない理由は、Embedで実現する際の入れ子関係は 以下の2パターンが考えられますが
+		//   xxxCarFactory{ abstractCarFactory } の構造だと 内側の abstractCarFactory上のテンプレートから上位の 実装関数が叩けません
+		//   abstractCarFactory{ xxxCarFactory } の構造だと 内側の 実装関数を叩けますが 疎結合のための interfaceが必要になります
 		carFactory
 	}
 )
