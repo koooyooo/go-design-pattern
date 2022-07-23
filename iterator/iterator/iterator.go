a55e6fd82daae09dc3beeb510b2b5228b923d5b4
koooyooo <jab.cross.hook@gmail.com>
2022-07-23 17:20:00 +0900

    enhance iterator explanation

--- iterator/iterator/iterator.go
+++ iterator/iterator/iterator.go
@@ -1,34 +1,3 @@
-// Package iterator
-//
-// [目的]
-// Iteratorの目的は、スライスの要素を安全に取り出すことです。
-//
-// [概要]
-// 通常のIndexを用いた取り出しは、スライスの要素数を超えパニックとなる可能性があります。
-// Iteratorを用いると、スライスの要素は内部管理され、この様な危険がありません。
-//
-//
-// [危うい要素アクセス]
-// for i := 0; i < len(array); i++ {
-//     fmt.Println(array[i])
-// }
-//
-// [安全な要素アクセス: Iterator]
-// for ite.HasNext() {
-//    fmt.Println(ite.Next())
-// }
-//
-// なお、Go言語の場合は rangeにより indexによる要素アクセスを回避可能なので Iteratorの役割は満たせていると言えます。
-//
-// [安全な要素アクセス: range]
-// for _, a := range array {
-//     fmt.Println(a)
-// }
-//
-// 但し、rangeを利用できるのは 配列・スライス・マップに限定されます。
-// Iteratorを使えば、言語機能に頼らずに様々なバリエーションで 安全な要素アクセスを実現することができます。
-// 例えば、ファイルから一行ずつ読み込む Iterator, DBから一レコードずつ読み込む Iterator, APIから1レスポンスずつ読み込む Iterator 等です。
-//
 package iterator
 
 // Iterator は、次の要素があるかどうかを返すイテレータインターフェイス
--- iterator/iterator_test.go
+++ iterator/iterator_test.go
@@ -1,3 +1,33 @@
+// Package iterator_pattern
+//
+// [目的]
+// Iteratorの目的は、スライスの要素を安全に取り出すことです。
+//
+// [概要]
+// 通常のIndexを用いた取り出しは、スライスの要素数を超えパニックとなる可能性があります。
+// Iteratorを用いると、スライスの要素は内部管理され、この様な危険がありません。
+//
+// [危うい要素アクセス]
+// for i := 0; i < len(array); i++ {
+//     fmt.Println(array[i])
+// }
+//
+// [安全な要素アクセス: Iterator]
+// for ite.HasNext() {
+//    fmt.Println(ite.Next())
+// }
+//
+// なお、Go言語の場合は rangeにより indexによる要素アクセスを回避可能なので Iteratorの役割は満たせていると言えます。
+//
+// [安全な要素アクセス: range]
+// for _, a := range array {
+//     fmt.Println(a)
+// }
+//
+// 但し、rangeを利用できるのは 配列・スライス・マップに限定されます。
+// Iteratorを使えば、言語機能に頼らずに様々なバリエーションで 安全な要素アクセスを実現することができます。
+// 例えば、ファイルから一行ずつ読み込む Iterator, DBから一レコードずつ読み込む Iterator, APIから1レスポンスずつ読み込む Iterator 等です。
+//
 package iterator_pattern
 
 import (
@@ -27,11 +57,14 @@ func TestIterator(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
+			// Iterator を生成する
 			ite := iterator.NewSliceIterator[int](test.origin)
 			var actual []int
+			// HasNext()と Next()を組み合わせることで indexアクセスせずにループ処理を実現する
 			for ite.HasNext() {
 				actual = append(actual, ite.Next())
 			}
+			// 元の要素が正しくコピーされていることを保証する。
 			assert.Equal(t, test.expected, actual)
 		})
 	}
