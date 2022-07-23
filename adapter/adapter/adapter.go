e56345aa4a8c4a290892dd6594588555ecfbcaaf
koooyooo <jab.cross.hook@gmail.com>
2022-07-24 07:31:38 +0900

    refactor adapter's comments

--- adapter/adapter/adapter.go
+++ adapter/adapter/adapter.go
@@ -7,33 +7,31 @@ import (
 )
 
 type (
-	// レガシーAPI
-	// (httpパッケージを活用せずに実装)
+	// LegacyAPI はレガシーなAPI (httpパッケージを活用せずに実装)
 	LegacyAPI interface {
 		Receive(method, url string, header map[string][]string, body []byte) (string, error)
 	}
-	// モダンAPI
-	// (httpパッケージを活用して実装)
+	// ModernAPI はモダンなAPI (httpパッケージを活用して実装)
 	ModernAPI interface {
 		Receive(r *http.Request) (string, error)
 	}
 )
 
-// レガシーAPI実装
+// LegacyAPIImpl はレガシーAPI実装
 type LegacyAPIImpl struct{}
 
-// Receive はレガシーAPIの実装です
+// Receive はレガシーAPIの実装
 func (l LegacyAPIImpl) Receive(method, url string, header map[string][]string, body []byte) (string, error) {
 	return fmt.Sprintf("legacy: %s %s", method, url), nil
 }
 
-// Adapter はアダプタの実装です。
+// Adapter はアダプタの実装
 type Adapter struct {
 	// 委譲対象のLegacyAPIを保持
 	Legacy LegacyAPI
 }
 
-// Receive は変換処理の実装でありモダンAPIのインターフェイスも満足させます。 LegacyAPIを上手に活用して機能を実現します。
+// Receive は変換処理の実装でありモダンAPIのインターフェイスも満足させる。LegacyAPIを上手に活用して機能を実現する。
 func (a Adapter) Receive(r *http.Request) (string, error) {
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
